fae4490c63a178b1f7d37b337b7979bd1f642b21
AndresVillamil <bcl.evillamil@gmail.com>
2022-06-16 08:21:40 -0500

    Actualiza URL seguridad

--- src/config/llaves.ts
+++ src/config/llaves.ts
@@ -1,4 +1,4 @@
 export namespace Llaves{
   export const claveJWT = 'JWT@2022*';
-  export const urlServiciosNotificaciones = 'http://localhost:5000/';
+  export const urlServiciosNotificaciones = 'https://warm-cove-38720.herokuapp.com/';
 }
